7cdba1cc58782736dc4ee875ee3e5b33cc199b20
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-13 23:36:24 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,8 +11,8 @@ export default function About() {
                 />
 
             <div className="items-center p-5 md:p-8 rounded-2xl max-w-lg text-left lg:ml-20 lg:-mt-6">
-                    <h3 className="text-center text-2xl font-semibold mb-4 lg:text-start">Who am I?</h3>
-                    <p className="text-center text-base md:text-base leading-relaxed lg:text-start">
+                    <h3 className="text-center text-2xl font-semibold mb-4 lg:text-start text-emerald-900">Who am I?</h3>
+                    <p className="text-center text-base md:text-base leading-relaxed lg:text-start text-emerald-800">
                         Hello! I am Umm-E-Hani a passionate learner on the path to becoming a full-stack developer. I am currently honing my skills in web technologies like TypeScript HTML CSS and JavaScript to build innovative web applications. Coding and solving complex problems are a big part of why I love development as it allows me to think critically and find practical solutions. <br /><br />
 
                         I also enjoy creating user-friendly designs ensuring the user experience is central to my work. As a student under the Governor Sindh IT Initiative I am continuously learning and applying new skills to my projects setting a strong foundation for my future. My goal is to build accessible applications that positively impact on peoples lives. With my passion for tech and commitment to growth I am excited to collaborate contribute and make a meaningful impact in the tech community.
