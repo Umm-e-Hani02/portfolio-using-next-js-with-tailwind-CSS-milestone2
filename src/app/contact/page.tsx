cb70365b9141dea1b09f29ee50217b886444efa9
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-14 00:13:25 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,9 +6,9 @@ export default function Contact(){
             <div className="items-center justify-center">
                 <h1 className="text-3xl font-bold mb-5 text-center text-emerald-900">Contact Me</h1>
                 <form action="mailto:hanirh2468@gmail.com" className="items-center justify-center">
-                    <input className="rounded-lg w-64 p-2 mb-3 focus:bg-gray-100 sm:w-[300px] text-black" type="text" placeholder="Your Name:" required/> <br />
-                    <input className="rounded-lg w-64 p-2 mb-3 focus:bg-gray-100 sm:w-[300px] text-black" type="email" placeholder="Your Email:" required/> <br />
-                    <textarea className="rounded-lg w-64 p-2 mb-3 focus:bg-gray-100 text-black sm:w-[300px]" name="message" id="message" rows={11} placeholder="Your message:"></textarea> <br />
+                    <input className="rounded-lg w-64 p-2 mb-3 focus:outline-emerald-900 focus:bg-gray-100 sm:w-[300px] text-black" type="text" placeholder="Your Name:" required/> <br />
+                    <input className="rounded-lg w-64 p-2 mb-3 focus:outline-emerald-900 focus:bg-gray-100 sm:w-[300px] text-black" type="email" placeholder="Your Email:" required/> <br />
+                    <textarea className="rounded-lg w-64 p-2 mb-3 focus:outline-emerald-900 focus:bg-gray-100 text-black sm:w-[300px]" name="message" id="message" rows={11} placeholder="Your message:"></textarea> <br />
                     <button className="flex gap-2 bg-white text-teal-500 p-2 rounded-3xl active:opacity-5 text-emerald-800">
                         Send message <FontAwesomeIcon icon={faPaperPlane} className="h-5"/>
                     </button>
