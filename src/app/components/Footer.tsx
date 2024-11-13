a9c2ee50375792e873940f65dcf3788271427bf6
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-13 23:52:11 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,14 +5,14 @@ export default function Footer(){
     return(
         <footer className="bg-gradient-to-r from-teal-500 via-green-400 to-lime-300 text-white p-6 text-center">
             <div className="mt-4">
-                <p className="flex items-center justify-center mb-2">
-                <FontAwesomeIcon icon={faEnvelope} className="h-6 mr-1" />
+                <p className="flex items-center justify-center mb-2 text-emerald-800">
+                <FontAwesomeIcon icon={faEnvelope} className="h-6 mr-1 " />
                 hanirh2468@gmail.com
                 </p>
-                <Link href="https://www.linkedin.com/in/umm-e-hani-400b812b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="mx-3">Linkedin</Link>
-                <Link href="https://github.com/Umm-e-Hani02" target="_blank" className="mx-3">Github</Link>
+                <Link href="https://www.linkedin.com/in/umm-e-hani-400b812b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="mx-3 text-emerald-800">Linkedin</Link>
+                <Link href="https://github.com/Umm-e-Hani02" target="_blank" className="mx-3 text-emerald-800">Github</Link>
             </div>
-            <div className="mt-4 text-sm">
+            <div className="mt-4 text-sm text-emerald-800">
                 <p>&copy; Umm-e-Hani 2024 | All rights reserved.</p> 
             </div>
         </footer>
