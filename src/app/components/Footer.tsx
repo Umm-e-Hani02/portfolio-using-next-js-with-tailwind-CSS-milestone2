c81a7d4823958f86de03a4dff63a46a6c1e4bd93
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-14 17:10:27 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,11 +3,13 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 export default function Footer(){
     return(
-        <footer className="bg-gradient-to-r from-teal-500 via-green-400 to-lime-300 text-white p-6 text-center">
+        <footer className="bg-gradient-to-r from-teal-600 via-green-500 to-lime-400 text-white p-6 text-center">
             <div className="mt-4">
                 <p className="flex items-center justify-center mb-2 text-emerald-800">
-                <FontAwesomeIcon icon={faEnvelope} className="h-6 mr-1 " />
-                hanirh2468@gmail.com
+                    <Link href="mailto:hanirh2468@gmail.com">
+                    <FontAwesomeIcon icon={faEnvelope} className="h-6 mr-1 " />
+                    </Link>
+                
                 </p>
                 <Link href="https://www.linkedin.com/in/umm-e-hani-400b812b4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="mx-3 text-emerald-800">Linkedin</Link>
                 <Link href="https://github.com/Umm-e-Hani02" target="_blank" className="mx-3 text-emerald-800">Github</Link>
